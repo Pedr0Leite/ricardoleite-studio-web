f1815a0507f4b9d5a0236442e837a378be0d29aa
Pedr0Leite <pedromiguelgoncalvesleite@gmail.com>
2023-10-18 00:37:15 +0100

    onhover fixed. changes to main page

--- components/MainTitle/MainTitle.styled.ts
+++ components/MainTitle/MainTitle.styled.ts
@@ -61,3 +61,80 @@ export const TopDiv = styled.div<colorInterface>`
         color: black;
     }
 `;
+
+export const TitleMobile = styled.div<colorInterface>`
+    font-family: "SuisseIntl-Regular";
+    // color:  ${props => props.color};
+    position: absolute;
+    display: inline;
+    left: 50%;
+    top:50%;
+    transform: translate(-50%, -50%);
+    white-space: nowrap;
+    font-size: 30vw;
+    font-style: normal;
+
+    & p {
+        line-height: 1;
+        text-align: center;
+        margin: 0;
+        color: black;
+    }
+
+    // @media only screen and (max-width: 850px) {
+    //     font-size: 160px;
+    //   }
+
+`;
+
+export const TopDivMobile = styled.div<colorInterface>`
+    // border-top: 1px solid ${props => props.color};
+    border-top: 1px solid black;
+    position: relative;
+    z-index: 2;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 15px;
+    padding-top: 10px;
+
+    & span {
+        // color:  ${props => props.color};
+        font-family: "SuisseIntl-Regular";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 25px;
+        line-height: 36px;
+        letter-spacing: 0.18px;
+        color: black;
+    }
+`;
+
+export const TopBottomMainDiv = styled.div`
+    display: flex;
+    width: 100%;
+    height: 88vh;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+export const BottomDivMobile = styled.div<colorInterface>`
+    // border-top: 1px solid ${props => props.color};
+    border-bottom: 1px solid black;
+    position: relative;
+    z-index: 2;
+    display: flex;
+    justify-content: space-between;
+    margin: 0 15px;
+    padding-top: 10px;
+
+    & span {
+        // color:  ${props => props.color};
+        font-family: "SuisseIntl-Regular";
+        font-style: normal;
+        font-weight: 500;
+        font-size: 25px;
+        line-height: 36px;
+        letter-spacing: 0.18px;
+        color: black;
+    }
+`;
